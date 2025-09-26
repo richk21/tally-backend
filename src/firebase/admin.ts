e29b2d44a9f1f8c6388a9c7d9198109be2a864b8
richa <richa21kiran@gmail.com>
2025-09-27 00:50:29 +0530

    ENV variable change

--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -1,5 +1,6 @@
 import admin from 'firebase-admin';
-import serviceAccount from './service-account-key.json';
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT || '{}');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
