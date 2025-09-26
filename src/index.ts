179b9f60d18f607f68ffc4eb3bd0b1e298a2e3d8
richa <richa21kiran@gmail.com>
2025-09-27 01:19:57 +0530

    cors and firebase config

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,26 @@ dotenv.config();
 import { setupSwagger } from './swagger';
 
 const app = express();
-app.use(cors());
+
+const allowedOrigins = ['https://tally-survey-app.netlify.app'];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = 'CORS policy does not allow access from this origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.use('/api/surveys', surveyRoutes);
 app.use('/api/localities', localitiesRoutes);
 
 setupSwagger(app);
-
-const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
